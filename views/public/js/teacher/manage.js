f12796d1aa9c63a08a5e017fc52b636b3d5309da
why <why@163.com>
2017-06-22 21:57:23 +0800

    第一次尝试做讲师

--- views/public/js/teacher/manage.js
+++ views/public/js/teacher/manage.js
@@ -1,7 +1,7 @@
 /**
  * Created by why on 2017/6/22.
  */
-define(["jquery","template","jqueryForm","utility"],function ($,template,form,utility) {
+define(["jquery","template","js/utility","form","datepicker","datepickerzh"],function ($,template,utility,form,dp,dpzh) {
     // var search = location.search;
     // console.log(search);
     // search = search.slice(1);
@@ -14,40 +14,80 @@ define(["jquery","template","jqueryForm","utility"],function ($,template,form,ut
     // console.log(searchObj);
     
 
+       //根据id发送请求，去服务器请求数据
+    //判断一下，如果有id，说明是一个编辑功能，没有id就是添加功能
+    if(utility.queryString().tc_id){
+        $.ajax({
+            url:'/api/teacher/edit',
+            type:'get',
+            data:{tc_id:utility.queryString().tc_id},
+            success:function (res){
+                if(res.code==200){
+                    res.result.title = "讲师编辑";
+                    res.result.saveBtnText = "保存"; //添加两个属性使页面更合理
+                    var htmlStr = template('tc_edit_tpl',res.result);
+                    // console.log(htmlStr);
+                    $('.teacher').html(htmlStr);
+                    //加载日期插件,通过属性选择器
+                    $("input[name=tc_join_date]").datepicker({
+                        format:"yyyy-mm-dd",
+                        language:"zh-CN"
 
-    $.ajax({
-        url:'/api/teacher/edit',
-        type:'get',
-        data:{tc_id:utility.queryString().tc_id},
-        success:function (res){
-            if(res.code==200){
-                res.result.title = "讲师编辑";
-                res.result.saveBtnText = "保存"; //添加两个属性使页面更合理
-                var htmlStr = template('tc_edit_tpl',res.result);
-                // console.log(htmlStr);
-                $('.teacher').html(htmlStr);
+                    });
+
+                }
             }
-        }
-    });
-    // $("form").ajaxForm({
-    //     url:"/api/teacher/update",
-    //     type:"post",
-    //     success:function (res1) {
-    //         alert(res1.msg)
-    //     }
-    // })
-  $(".teacher").on("click","btn-success",function () {
-      $("form").ajaxSubmit({
-          url:"/api/teacher/update",
-          type:"post",
+        });
+        submitAjax("/api/teacher/update")
+    }
+    else{
+        var htmlStr = template("tc_edit_tpl",{
+            title:"讲师编辑",
+            saveBtnText:"添加",
+            tc_gender:1
+        });
+        $('.teacher').html(htmlStr);
+        submitAjax("/api/teacher/add")
+    }
 
-          success:function () {
-              alert(123)
+  // $(".teacher").on("click",".btn-addInfo",function () {
+  //     $("form").ajaxSubmit({
+  //         url:"/api/teacher/update",
+  //         type:"post",
+  //
+  //         success:function () {
+  //             alert("提交成功！");
+  //             location.href = "/teacher/teacher_list"
+  //
+  //         }
+  //
+  //     })
+  // });
 
-          }
 
-      })
-  })
+  //添加和编辑都是提交数据，可以定义一个提交函数
+function  submitAjax(url) {
+    $(".teacher").on("click","btn-addInfo",function () {
+        $("form").ajaxSubmit({
+            url:url,
+            type:"post",
+            success:function (res) {
+                alert("提交成功！");
+                location.href="/teacher/teacher_list"
+            }
+        });
+        return false;
+    })
+}
+
+
+
+
+
+
+
+
+
 
 
 
--- views/public/libs/config.js
+++ views/public/libs/config.js
@@ -8,17 +8,22 @@
             // "template":"assets/artTemplate/template-native",
             "template":"assets/artTemplate/template",
             "nprogress":"assets/nprogress/nprogress",
-            "jqueryForm":"assets/jquery-form/jquery.form",
             "region":"assets/jquery-region/jquery.region",
             "common":"js/dashboard/common",
             "login":"js/dashboard/login",
-            "utility":"js/utility"
+            "form":"assets/jquery-form/jquery.form",
+            "datepicker":"assets/bootstrap-datepicker/js/bootstrp-datepicker",
+            'datepickerzh':'assets/bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min'
+
 
         },
         shim:{
             "bootstrap":{deps:["jquery"]},
             "cookie":{deps:["jquery"]},
 
+        },
+        'datepickerzh':{
+            deps:['jquery']
         }
     })
 
